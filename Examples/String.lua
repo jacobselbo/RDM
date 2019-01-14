390985874258ce838bb4f7faa48dc38de0807584
froghopperjacob <froghopperjacob@gmail.com>
2019-01-13 21:18:14 -0500

    Examples, Fix travis

---
+++ Examples/String.lua
@@ -0,0 +1,22 @@
+local RDMF = game:GetService("ServerScriptService"):WaitForChild("RDM"):WaitForChild("Source")
+local RDM = require(RDMF:WaitForChild("MainModule"))(script)
+
+local players = game:GetService("Players")
+
+local prefix = "$"
+local dem = "/"
+
+local stringUtil = RDM:Import("String")
+
+players.PlayerAdded:Connect(function(player)
+	player.Chatted:connect(function(message)
+		if (stringUtil:Starts(prefix)) then
+			local message = string.sub(message, prefix:len())
+			local arguments = stringUtil:Split(message, dem)
+			
+			if (arguments[1] == "kill") then
+				players[arguments[2]].Character:BreakJoints()
+			end
+		end
+	end)
+end)
