0e9c1eb1a2487981bfd53dd9d04871ed48c5b035
froghopperjacob <froghopperjacob@gmail.com>
2019-01-14 21:24:03 -0500

    My spelling sucks. Fix spec

--- RDMTests/String.spec.lua
+++ RDMTests/String.spec.lua
@@ -9,31 +9,31 @@ return function()
 		it(string.format(shouldGive, "Starts"), function()
 			local boolean = stringUtil:Starts("$test", "$")
 
-			except(boolean).to.equal(true)
+			expect(boolean).to.equal(true)
 		end)
 
 		it(string.format(shouldGive, "Ends"), function()
 			local boolean = stringUtil:Ends("test$", "$")
 
-			except(boolean).to.equal(true)
+			expect(boolean).to.equal(true)
 		end)
 
 		it(string.format(shouldGive, "IsLower"), function()
 			local boolean = stringUtil:IsLower("test")
 
-			except(boolean).to.equal(true)
+			expect(boolean).to.equal(true)
 		end)
 
 		it(string.format(shouldGive, "IsUpper"), function()
 			local boolean = stringUtil:IsUpper("TEST")
 
-			except(boolean).to.eqaul(true)
+			expect(boolean).to.eqaul(true)
 		end)
 
 		it(string.format(shouldGive, "ToBoolean"), function()
 			local boolean = stringUtil:ToBoolean("true")
 
-			except(boolean).to.equal(true)
+			expect(boolean).to.equal(true)
 		end)
 	end)
 
@@ -41,31 +41,31 @@ return function()
 		it(string.format(shouldBe, "Trim"), function()
 			local trimmed = stringUtil:Trim("     test     ")
 
-			except(trimmed).to.equal("test")
+			expect(trimmed).to.equal("test")
 		end)
 
 		it(string.format(shouldBe, "Join"), function()
 			local joined = stringUtil:Join({ "hi", "my", "name", "is", "bob" }, " ")
 
-			except(joined).to.equal("hi my name is bob")
+			expect(joined).to.equal("hi my name is bob")
 		end)
 
 		it(string.format(shouldBe, "Split"), function()
 			local splitTotal = #stringUtil:Split("test-test-test")
 
-			except(splitTotal).to.equal(3)
+			expect(splitTotal).to.equal(3)
 		end)
 
 		it(string.format(shouldBe, "Count"), function()
 			local countTotal = stringUtil:Count("hihihihi")
 
-			except(countTotal).to.equal(4)
+			expect(countTotal).to.equal(4)
 		end)
 
 		it(string.format(shouldBe, "DecodeHTMLEntities"), function()
 			local decoded = stringUtil:DecodeHTMLEntities("John&amp;John")
 
-			except(decoded).to.equal("John&John")
+			expect(decoded).to.equal("John&John")
 		end)
 	end)
 end
